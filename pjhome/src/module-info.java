ecf7370bbb6cc79eed7aa1e53174c1ee97e1849f
ADMIN <ADMIN@QA-NINJA-1>
2021-09-07 21:50:49 -0400

    First Commit

---
+++ pjhome/src/module-info.java
@@ -0,0 +1,2 @@
+module pjhome {
+}
---
+++ pjhome/src/pjhome/Peanut.java
@@ -0,0 +1,23 @@
+package pjhome;
+
+public class Peanut {
+
+	// water the plant
+
+	public static void main(String[] args) {
+	Peanut cat =  new Peanut();
+cat.stonyfield();
+	}
+
+	public void stonyfield() {
+
+		System.out.print("Peanut Butter");
+
+	}
+	
+	
+	
+}
+
+
+
